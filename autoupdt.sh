51e33c71f43252848ed23487051f45898d9d07cf
goekem <maxattack222@gmail.com>
2020-05-09 12:51:04 -0500

    Update autoupdt.sh
    apt-get to apt

--- autoupdt.sh
+++ autoupdt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-apt-get update
-apt-get -y upgrade
-apt-get -y dist-upgrade
-apt-get clean
-apt-get -y autoremove
+apt update
+apt -y upgrade
+apt -y dist-upgrade
+apt clean
+apt -y autoremove
